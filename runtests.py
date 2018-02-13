10b21f7eb1d9ec01fbfa87c9827efa58ade3510c
Martin Mahner <martin@mahner.org>
2018-02-13 13:07:42 +0100

    Use script to run tests. Sanity check for Travis versions.

---
+++ runtests.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import sys
+
+from django import setup
+from django.conf import settings
+from django.test.runner import DiscoverRunner
+
+def runtests(*test_args):
+    # Setup settings
+    if not settings.configured:
+        from eventlog.tests.testapp import settings as TEST_SETTINGS
+        settings.configure(**TEST_SETTINGS.__dict__)
+    setup()
+    test_runner = DiscoverRunner(verbosity=1)
+    failures = test_runner.run_tests(['eventlog'])
+    if failures:
+        sys.exit(failures)
+
+if __name__ == '__main__':
+    runtests(*sys.argv[1:])
